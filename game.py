6ff4d6faf217cd0cf5d12adaa8f82ff1ac7d520c
Official21A <officialamirhossein21@gmail.com>
2020-09-26 08:15:58 +0330

    Refactoring

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import random
 from headers import __find__
 
 
-word_list_data = ['data', 'port', 'admin']
+word_list_data = ['data', 'port', 'admin'] # data words for the game
 
 main_word = random.choice(word_list_data)
 
@@ -12,21 +12,28 @@ counter = 0
 game = True
 
 while game == True:
+	# main while of the game
 	char = input("Guess >> ")
+
 	if len(char) == 1:
 		indexes = __find__(main_word, char)
+
 		if len(indexes) != 0:
+			# the string contains the user char input
 			print(f"It has {len(indexes)} {char}.")
 			counter += len(indexes)
 		else:
+			# no char in string
 			print(f"No {char}.")	
 	else:
+		# less or more than one words compare differently
 		if char == main_word:
 			print("Correct")
 			game = False
 		else:
 			print("Incorrect")	
+			
 	if counter == word_len:
 		game = False
 
-print("Finish")						
+print(main_word)						
